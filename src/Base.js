a23294481551763b93b65c60d0a338bdef8115f6
ThePersigg <49908303+perssBest@users.noreply.github.com>
2020-12-03 22:04:49 +0500

    ебал я в рот ваш http

--- src/Base.js
+++ src/Base.js
@@ -1,41 +1,23 @@
-const http = require("http")
+const fetch = require("node-fetch")
 const config = require("./config").config;
 
 class MissAgent {
     constructor(APIKEY) {
         this.token = APIKEY;
         if(!this.token) throw new Error("Specify API-KEY. Documentation: https://github.com/miss-api-xyz/miss-api.js")
-
-        //
-
-        this.request = function(endpoint, options){
-            return new Promise((resolve, reject) => {
-                http.get(`${config.baseURL}/api/${config.APIversion}/${endpoint}`, options, (res) => {
-                    let body = " ";
-                    res.on('data', function(chunk){
-                        body += chunk;
-                        return resolve(JSON.parse(body))
-                    })
-                })
-            })
-        }
-
-        //
-
     }
 
     get = async function(endpoint) {
         if(!endpoint) throw new Error("Specify an available endpoint. Documentation: https://github.com/miss-api-xyz/miss-api.js")
-        return this.request(endpoint, {
+        let res = await fetch(`${config.baseURL}/api/${config.APIversion}/${endpoint}`, {
             headers: {
                 "Content-Type": "application/json",
                 "Authorization": this.token,
             },
         })
+        return res.json()
     }
-
 }
 
 module.exports = MissAgent;
-
 /* Developer with ❤ by miss-api Team */
